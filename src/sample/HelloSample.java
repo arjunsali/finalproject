d345fb41934ecc2e3e5ae081a5ca8cdce0b0cbdf
arjun <arjunprasad73@gmail.com>
2023-04-02 17:39:42 +0530

    new file updated

---
+++ src/sample/HelloSample.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.util.Scanner;
+
+public class HelloSample {
+	public static void main(String ar[]) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a 2 numbers:");
+		
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=a+b;
+		System.out.println("Result is: "+c);
+		
+	}
+
+}
